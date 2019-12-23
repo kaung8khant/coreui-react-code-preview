1621ffe8b2db376e4b77023523135e4a5ffb2619
phyuthinnaung4 <phyuthinn.aung@flymya.co>
2019-12-23 14:45:22 +0630

    update code preview for base

--- src/views/Base/Tooltips/Tooltips.js
+++ src/views/Base/Tooltips/Tooltips.js
@@ -1,29 +1,50 @@
-import React, { Component } from 'react';
-import { Button, Card, CardBody, CardHeader, Tooltip, UncontrolledTooltip } from 'reactstrap';
+import React, { Component } from "react";
+import {
+  Button,
+  Card,
+  CardBody,
+  CardHeader,
+  Tooltip,
+  UncontrolledTooltip,
+  Col
+} from "reactstrap";
+import { PrismLight as SyntaxHighlighter } from "react-syntax-highlighter";
+import { tomorrow } from "react-syntax-highlighter/dist/esm/styles/prism";
+import jsx from "react-syntax-highlighter/dist/esm/languages/prism/jsx";
 
+SyntaxHighlighter.registerLanguage("jsx", jsx);
 class TooltipItem extends React.Component {
   constructor(props) {
     super(props);
 
     this.toggle = this.toggle.bind(this);
     this.state = {
-      tooltipOpen: false,
+      tooltipOpen: false
     };
   }
 
   toggle() {
     this.setState({
-      tooltipOpen: !this.state.tooltipOpen,
+      tooltipOpen: !this.state.tooltipOpen
     });
   }
 
   render() {
     return (
       <span>
-        <Button className="mr-1" color="secondary" id={'Tooltip-' + this.props.id}>
+        <Button
+          className="mr-1"
+          color="secondary"
+          id={"Tooltip-" + this.props.id}
+        >
           {this.props.item.text}
         </Button>
-        <Tooltip placement={this.props.item.placement} isOpen={this.state.tooltipOpen} target={'Tooltip-' + this.props.id} toggle={this.toggle}>
+        <Tooltip
+          placement={this.props.item.placement}
+          isOpen={this.state.tooltipOpen}
+          target={"Tooltip-" + this.props.id}
+          toggle={this.toggle}
+        >
           Tooltip Content!
         </Tooltip>
       </span>
@@ -32,7 +53,6 @@ class TooltipItem extends React.Component {
 }
 
 class Tooltips extends Component {
-
   constructor(props) {
     super(props);
 
@@ -41,42 +61,64 @@ class Tooltips extends Component {
       tooltipOpen: [false, false],
       tooltips: [
         {
-          placement: 'top',
-          text: 'Top',
+          placement: "top",
+          text: "Top"
         },
         {
-          placement: 'bottom',
-          text: 'Bottom',
+          placement: "bottom",
+          text: "Bottom"
         },
         {
-          placement: 'left',
-          text: 'Left',
+          placement: "left",
+          text: "Left"
         },
         {
-          placement: 'right',
-          text: 'Right',
-        },
+          placement: "right",
+          text: "Right"
+        }
       ],
+      code: false
     };
   }
 
   toggle(i) {
     const newArray = this.state.tooltipOpen.map((element, index) => {
-      return (index === i ? !element : false);
+      return index === i ? !element : false;
     });
     this.setState({
-      tooltipOpen: newArray,
+      tooltipOpen: newArray
     });
   }
 
   render() {
     return (
       <div className="animated fadeIn">
+        <Col md="2" xl="2" style={{ marginBottom: "20px" }}>
+          <Button
+            block
+            outline
+            color="light"
+            onClick={() => this.setState({ code: !this.state.code })}
+          >
+            {this.state.code ? "Close" : "</> Code"}
+          </Button>
+        </Col>
+        {this.state.code && (
+          <SyntaxHighlighter language="jsx" style={tomorrow}>
+            {codeString}
+          </SyntaxHighlighter>
+        )}
         <Card>
           <CardHeader>
-            <i className="fa fa-align-justify"></i><strong>Tooltips</strong>
+            <i className="fa fa-align-justify"></i>
+            <strong>Tooltips</strong>
             <div className="card-header-actions">
-              <a href="https://reactstrap.github.io/components/tooltips/" rel="noreferrer noopener" target="_blank" className="card-header-action">
+              <a
+                href="https://reactstrap.github.io/components/tooltips/"
+                rel="noreferrer noopener"
+                target="_blank"
+                className="card-header-action"
+              >
                 <small className="text-muted">docs</small>
               </a>
             </div>
@@ -84,27 +126,44 @@ class Tooltips extends Component {
           <CardBody>
             {/*eslint-disable-next-line*/}
             <p>Somewhere in here is a <a href="#" id="TooltipExample">tooltip</a>.</p>
-            <Tooltip placement="right" isOpen={this.state.tooltipOpen[0]} target="TooltipExample" toggle={() => {this.toggle(0);}}>
+            <Tooltip
+              placement="right"
+              isOpen={this.state.tooltipOpen[0]}
+              target="TooltipExample"
+              toggle={() => {
+                this.toggle(0);
+              }}
+            >
               Hello world!
             </Tooltip>
           </CardBody>
         </Card>
         <Card>
           <CardHeader>
-            <i className="fa fa-align-justify"></i><strong>Tooltip</strong>
+            <i className="fa fa-align-justify"></i>
+            <strong>Tooltip</strong>
             <small> disable autohide</small>
           </CardHeader>
           <CardBody>
             {/*eslint-disable-next-line*/}
             <p>Sometimes you need to allow users to select text within a <a href="#" id="DisabledAutoHideExample">tooltip</a>.</p>
-            <Tooltip placement="top" isOpen={this.state.tooltipOpen[1]} autohide={false} target="DisabledAutoHideExample" toggle={() => {this.toggle(1);}}>
+            <Tooltip
+              placement="top"
+              isOpen={this.state.tooltipOpen[1]}
+              autohide={false}
+              target="DisabledAutoHideExample"
+              toggle={() => {
+                this.toggle(1);
+              }}
+            >
               Try to select this text!
             </Tooltip>
           </CardBody>
         </Card>
         <Card>
           <CardHeader>
-            <i className="fa fa-align-justify"></i><strong>Tooltip</strong>
+            <i className="fa fa-align-justify"></i>
+            <strong>Tooltip</strong>
             <small> list</small>
           </CardHeader>
           <CardBody>
@@ -115,13 +174,17 @@ class Tooltips extends Component {
         </Card>
         <Card>
           <CardHeader>
-            <i className="fa fa-align-justify"></i><strong>Tooltip</strong>
+            <i className="fa fa-align-justify"></i>
+            <strong>Tooltip</strong>
             <small> uncontrolled</small>
           </CardHeader>
           <CardBody>
             {/*eslint-disable-next-line*/}
             <p>Somewhere in here is a <a href="#" id="UncontrolledTooltipExample">tooltip</a>.</p>
-            <UncontrolledTooltip placement="right" target="UncontrolledTooltipExample">
+            <UncontrolledTooltip
+              placement="right"
+              target="UncontrolledTooltipExample"
+            >
               Hello world!
             </UncontrolledTooltip>
           </CardBody>
@@ -131,4 +194,181 @@ class Tooltips extends Component {
   }
 }
 
+const codeString = `
+import React, { Component } from "react";
+import {
+  Button,
+  Card,
+  CardBody,
+  CardHeader,
+  Tooltip,
+  UncontrolledTooltip
+} from "reactstrap";
+
+class TooltipItem extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.toggle = this.toggle.bind(this);
+    this.state = {
+      tooltipOpen: false
+    };
+  }
+
+  toggle() {
+    this.setState({
+      tooltipOpen: !this.state.tooltipOpen
+    });
+  }
+
+  render() {
+    return (
+      <span>
+        <Button
+          className="mr-1"
+          color="secondary"
+          id={"Tooltip-" + this.props.id}
+        >
+          {this.props.item.text}
+        </Button>
+        <Tooltip
+          placement={this.props.item.placement}
+          isOpen={this.state.tooltipOpen}
+          target={"Tooltip-" + this.props.id}
+          toggle={this.toggle}
+        >
+          Tooltip Content!
+        </Tooltip>
+      </span>
+    );
+  }
+}
+
+class Tooltips extends Component {
+  constructor(props) {
+    super(props);
+
+    this.toggle = this.toggle.bind(this);
+    this.state = {
+      tooltipOpen: [false, false],
+      tooltips: [
+        {
+          placement: "top",
+          text: "Top"
+        },
+        {
+          placement: "bottom",
+          text: "Bottom"
+        },
+        {
+          placement: "left",
+          text: "Left"
+        },
+        {
+          placement: "right",
+          text: "Right"
+        }
+      ]
+    };
+  }
+
+  toggle(i) {
+    const newArray = this.state.tooltipOpen.map((element, index) => {
+      return index === i ? !element : false;
+    });
+    this.setState({
+      tooltipOpen: newArray
+    });
+  }
+
+  render() {
+    return (
+      <div className="animated fadeIn">
+        <Card>
+          <CardHeader>
+            <i className="fa fa-align-justify"></i>
+            <strong>Tooltips</strong>
+            <div className="card-header-actions">
+              <a
+                href="https://reactstrap.github.io/components/tooltips/"
+                rel="noreferrer noopener"
+                target="_blank"
+                className="card-header-action"
+              >
+                <small className="text-muted">docs</small>
+              </a>
+            </div>
+          </CardHeader>
+          <CardBody>
+            {/*eslint-disable-next-line*/}
+            <p>Somewhere in here is a <a href="#" id="TooltipExample">tooltip</a>.</p>
+            <Tooltip
+              placement="right"
+              isOpen={this.state.tooltipOpen[0]}
+              target="TooltipExample"
+              toggle={() => {
+                this.toggle(0);
+              }}
+            >
+              Hello world!
+            </Tooltip>
+          </CardBody>
+        </Card>
+        <Card>
+          <CardHeader>
+            <i className="fa fa-align-justify"></i>
+            <strong>Tooltip</strong>
+            <small> disable autohide</small>
+          </CardHeader>
+          <CardBody>
+            {/*eslint-disable-next-line*/}
+            <p>Sometimes you need to allow users to select text within a <a href="#" id="DisabledAutoHideExample">tooltip</a>.</p>
+            <Tooltip
+              placement="top"
+              isOpen={this.state.tooltipOpen[1]}
+              autohide={false}
+              target="DisabledAutoHideExample"
+              toggle={() => {
+                this.toggle(1);
+              }}
+            >
+              Try to select this text!
+            </Tooltip>
+          </CardBody>
+        </Card>
+        <Card>
+          <CardHeader>
+            <i className="fa fa-align-justify"></i>
+            <strong>Tooltip</strong>
+            <small> list</small>
+          </CardHeader>
+          <CardBody>
+            {this.state.tooltips.map((tooltip, i) => {
+              return <TooltipItem key={i} item={tooltip} id={i} />;
+            })}
+          </CardBody>
+        </Card>
+        <Card>
+          <CardHeader>
+            <i className="fa fa-align-justify"></i>
+            <strong>Tooltip</strong>
+            <small> uncontrolled</small>
+          </CardHeader>
+          <CardBody>
+            {/*eslint-disable-next-line*/}
+            <p>Somewhere in here is a <a href="#" id="UncontrolledTooltipExample">tooltip</a>.</p>
+            <UncontrolledTooltip
+              placement="right"
+              target="UncontrolledTooltipExample"
+            >
+              Hello world!
+            </UncontrolledTooltip>
+          </CardBody>
+        </Card>
+      </div>
+    );
+  }
+}
+export default Tooltips;
+`;
 export default Tooltips;
